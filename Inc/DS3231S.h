b9f9f7296328d84c6ff2fa2449e0a1160e14b344
bant <bant62@gmail.com>
2017-08-27 10:28:22 +0900

    first add

---
+++ Inc/DS3231S.h
@@ -0,0 +1,40 @@
+#ifndef __DS3231S__H__
+#define __DS3231S__H__
+
+#include <stdbool.h>
+
+#define DS3231S_ADDR	0x68
+#define RETRY_DS3231S	3
+
+typedef struct
+{
+    uint8_t sec;		// 0 to 59
+    uint8_t min;		// 0 to 59
+    uint8_t hour;		// 12時間制なら(0から11) 24時間制なら(0から23)
+    uint8_t day;		// 1 to 31
+    uint8_t	month;		// 1 to 12
+    uint16_t year;		// 2000 to
+    uint8_t wday;		// 1 to 7
+
+    bool	isTwelve;	// TRUE なら 12時間制
+    bool	isAM;		// TRUE for AM, FALSE for PM
+} RTC_TIME;
+
+enum RTC_SQW_FREQ { FREQ_1 = 0, FREQ_1024, FREQ_4096, FREQ_8192 };
+
+/*======================================*/
+/*  関数定義						    */
+/*======================================*/
+extern uint8_t DS3231S_adjust(RTC_TIME *time);
+extern uint8_t DS3231S_now(RTC_TIME *time);
+extern uint8_t DS3231S_SQW_enable(bool enable);
+extern uint8_t DS3231S_SQW_set_freq(enum RTC_SQW_FREQ freq);
+extern uint8_t DS3231S_osc32kHz_enable(bool enable);
+extern void DS3231S_reset_alarm(void);
+extern void DS3231S_set_alarm_s(uint8_t hour, uint8_t min, uint8_t sec);
+extern void DS3231S_set_alarm(RTC_TIME *tm_);
+extern void DS3231S_get_alarm_s(uint8_t* hour, uint8_t* min, uint8_t* sec);
+extern void DS3231S_get_alarm(RTC_TIME* _tm);
+extern bool DS3231S_check_alarm(void);
+
+#endif
